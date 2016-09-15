693507f3315266d1c41f39ef32646551905fb1bc
Ian Partridge <i.partridge@uk.ibm.com>
2016-09-15 14:47:40 +0100

    updated dependency versions in Package.swift (#18)
    set version of https://github.com/IBM-Swift/Kitura-Session.git to 0.32.0
    set version of https://github.com/IBM-Swift/Kitura-redis.git to 0.32.0

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Session-Redis",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 0, minor: 31),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 0, minor: 30),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 0, minor: 32),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 0, minor: 32),
     ]
 )
