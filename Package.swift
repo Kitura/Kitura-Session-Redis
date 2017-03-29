4affcab5583227e8e1900952ad6ef9d1d290e824
Shmuel Kallner <kallner@il.ibm.com>
2017-03-29 16:38:15 +0300

    IBM-Swift/Kitura#1047 Updates to Kitura-Session-Redis for Swift 3.1

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corporation 2016
+ * Copyright IBM Corporation 2016, 2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Session-Redis",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 6)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Session.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 7)
     ]
 )
